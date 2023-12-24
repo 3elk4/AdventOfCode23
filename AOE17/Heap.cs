cd037fcf172803b9f949aa7c1deee7037d640e53
Izabela Wiatrowska <33397049+3elk4@users.noreply.github.com>
2023-12-24 01:24:45 +0100

    add aoe17 solution

---
+++ AOE17/Heap.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOE17
+{
+    public abstract class Heap<T> : IEnumerable<T>
+    {
+        private const int InitCap = 0;
+        private const int GrowFactor = 2;
+        private const int MinGrow = 1;
+
+        private int _capacity = InitCap;
+        private T[] _heap = new T[InitCap];
+        private int _count = 0;
+
+        public int Count { get { return _count; } }
+        public int Capacity { get { return _capacity; } }
+
+
+        protected Comparer<T> Comparer { get; private set; }
+
+        protected Heap() : this(Comparer<T>.Default) { }
+
+        protected Heap(Comparer<T> comparer) : this(Enumerable.Empty<T>(), comparer) { }
+
+        protected Heap(IEnumerable<T> collection) : this(collection, Comparer<T>.Default) { }
+
+        protected Heap(IEnumerable<T> collection, Comparer<T> comparer)
+        {
+            if (collection == null) throw new ArgumentNullException("Collection");
+            if (comparer == null) throw new ArgumentNullException("Comparer");
+
+            Comparer = comparer;
+
+            foreach (var item in collection)
+            {
+                if (_count == _capacity)
+                    Grow();
+
+                _heap[_count++] = item;
+            }
+
+            for (int i = Parent(_count - 1); i >= 0; i--)
+                Sink(i);
+        }
+
+        private void Grow()
+        {
+            int newCapacity = _capacity * GrowFactor + MinGrow;
+            var newHeap = new T[newCapacity];
+            Array.Copy(_heap, newHeap, _capacity);
+            _heap = newHeap;
+            _capacity = newCapacity;
+        }
+
+        private void Swap(int idx1, int idx2)
+        {
+            T tmp = _heap[idx1];
+            _heap[idx1] = _heap[idx2];
+            _heap[idx2] = tmp;
+        }
+        /// <summary>
+        /// Shifting down value in heap.
+        /// </summary>
+        /// <param name="idx"> Index of value to sink. </param>
+        private void Sink(int idx)
+        {
+            int domIdx = FindIndexOfDominatingNode(idx);
+
+            if (domIdx == idx) return;
+            Swap(idx, domIdx);
+            Sink(domIdx);
+        }
+
+        private int FindIndexOfDominatingNode(int idx)
+        {
+            int dominatingNode = idx;
+            dominatingNode = GetIndexOfDominatingNode(LeftChild(idx), dominatingNode);
+            dominatingNode = GetIndexOfDominatingNode(RightChild(idx), dominatingNode);
+
+            return dominatingNode;
+        }
+
+        private int GetIndexOfDominatingNode(int newIdx, int currentIdx)
+        {
+            return (newIdx < _count && !CheckProperOrder(_heap[currentIdx], _heap[newIdx])) ? newIdx : currentIdx;
+        }
+
+        /// <summary>
+        /// Shifting up value in heap
+        /// </summary>
+        /// <param name="idx"> Index of value to swim. </param>
+        private void Swim(int idx)
+        {
+            if (idx == 0 || CheckProperOrder(_heap[Parent(idx)], _heap[idx])) return;
+
+            Swap(idx, Parent(idx));
+            Swim(Parent(idx));
+        }
+
+        private int Parent(int idx)
+        {
+            return (idx + 1) / 2 - 1;
+        }
+
+        private int LeftChild(int idx)
+        {
+            return (idx + 1) * 2 - 1;
+        }
+
+        private int RightChild(int idx)
+        {
+            return LeftChild(idx) + 1;
+        }
+
+        protected abstract bool CheckProperOrder(T x, T y);
+
+        public T Extract()
+        {
+            if (_count == 0) throw new InvalidOperationException("Heap is empty");
+            T result = _heap[0];
+            _count--;
+            Swap(_count, 0);
+            Sink(0);
+            return result;
+        }
+
+        /// <summary>
+        /// Takes first element of heap to see it, not to extract it. To extract see extract method.
+        /// </summary>
+        /// <returns> Top of heap. </returns>
+        public T Peek()
+        {
+            if (_count == 0) return default(T);
+            return _heap[0];
+        }
+
+        public void Insert(T item)
+        {
+            if (_count == _capacity)
+                Grow();
+
+            _heap[_count++] = item;
+            Swim(_count - 1);
+        }
+
+        public void Replace(T item)
+        {
+            Extract();
+            Insert(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _heap.Take(Count).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MinHeap<T> : Heap<T>
+    {
+        public MinHeap() : this(Comparer<T>.Default) { }
+
+        public MinHeap(Comparer<T> comparer) : base(comparer) { }
+
+        public MinHeap(IEnumerable<T> collection) : base(collection) { }
+
+        public MinHeap(IEnumerable<T> collection, Comparer<T> comparer) : base(collection, comparer) { }
+
+        protected override bool CheckProperOrder(T x, T y)
+        {
+            return Comparer.Compare(x, y) <= 0;
+        }
+    }
+
+    public class MaxHeap<T> : Heap<T>
+    {
+        public MaxHeap() : this(Comparer<T>.Default) { }
+
+        public MaxHeap(Comparer<T> comparer) : base(comparer) { }
+
+        public MaxHeap(IEnumerable<T> collection) : base(collection) { }
+
+        public MaxHeap(IEnumerable<T> collection, Comparer<T> comparer) : base(collection, comparer) { }
+
+        protected override bool CheckProperOrder(T x, T y)
+        {
+            return Comparer.Compare(x, y) <= 0;
+        }
+    }
+}
---
+++ AOE17/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace AOE17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result1 = 0;
+            long result2 = 0;
+
+            string fileloc = @"data\input.txt";
+            int[][] grid = File.ReadAllLines(fileloc).Select(line => line.ToCharArray().Select(ch => int.Parse(ch.ToString())).ToArray()).ToArray();
+
+            //part 1
+            result1 = MinimizeHeatLoss(grid, (0, 0, 0, 0, 0, 0));
+            result2 = MinimizeHeatLoss2(grid, (0, 0, 0, 0, 0, 0));
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+
+        //should be able to use class instead of tuples but now is simple and special comparison for class is needed (heap comparison)
+
+        static private List<(int X, int Y)> dirs = new List<(int X, int Y)>() { (0, 1), (1, 0), (0, -1), (-1, 0) };
+
+        static public long MinimizeHeatLoss(int[][] grid, (int HL, int X, int Y, int DirX, int DirY, int Steps) startValue)
+        {
+            HashSet<(int X, int Y, int DirX, int DirY, int Steps)> seen = new HashSet<(int, int, int, int, int)>();
+
+            MinHeap<(int HL, int X, int Y, int DirX, int DirY, int Steps)> minHeap = new MinHeap<(int HL, int X, int Y, int DirX, int DirY, int Steps)>();
+            minHeap.Insert(startValue);
+
+            while (minHeap.Count > 0)
+            {
+                var item = minHeap.Extract();
+                var s = (item.X, item.Y, item.DirX, item.DirY, item.Steps);
+
+                if (item.Y == grid.Length - 1 && item.X == grid[0].Length - 1) return item.HL;
+                if (seen.Contains(s)) continue;
+
+                seen.Add(s);
+
+                if (item.Steps < 3 && (item.DirX, item.DirY) != (0, 0))
+                {
+                    (int X, int Y) newPos = (item.X + item.DirX, item.Y + item.DirY);
+
+                    if (newPos.X >= 0 && newPos.X < grid[0].Length && newPos.Y >= 0 && newPos.Y < grid.Length)
+                    {
+                        minHeap.Insert((item.HL + grid[newPos.Y][newPos.X], newPos.X, newPos.Y, item.DirX, item.DirY, item.Steps + 1));
+                    }
+                }
+
+                foreach (var dir in dirs)
+                {
+                    if (dir != (item.DirX, item.DirY) && dir != (-item.DirX, -item.DirY))
+                    {
+                        (int X, int Y) newPos = (item.X + dir.X, item.Y + dir.Y);
+
+                        if (newPos.X >= 0 && newPos.X < grid[0].Length && newPos.Y >= 0 && newPos.Y < grid.Length)
+                        {
+                            minHeap.Insert((item.HL + grid[newPos.Y][newPos.X], newPos.X, newPos.Y, dir.X, dir.Y, 1));
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        static public long MinimizeHeatLoss2(int[][] grid, (int HL, int X, int Y, int DirX, int DirY, int Steps) startValue)
+        {
+            HashSet<(int X, int Y, int DirX, int DirY, int Steps)> seen = new HashSet<(int, int, int, int, int)>();
+
+            MinHeap<(int HL, int X, int Y, int DirX, int DirY, int Steps)> minHeap = new MinHeap<(int HL, int X, int Y, int DirX, int DirY, int Steps)>();
+            minHeap.Insert(startValue);
+
+            while (minHeap.Count > 0)
+            {
+                var item = minHeap.Extract();
+                var s = (item.X, item.Y, item.DirX, item.DirY, item.Steps);
+
+                if (item.Y == grid.Length - 1 && item.X == grid[0].Length - 1 && item.Steps >= 4) return item.HL;
+                if (seen.Contains(s)) continue;
+
+                seen.Add(s);
+
+                if (item.Steps < 10 && (item.DirX, item.DirY) != (0, 0))
+                {
+                    (int nx, int ny) newPos = (item.X + item.DirX, item.Y + item.DirY);
+
+                    if (newPos.nx >= 0 && newPos.nx < grid[0].Length && newPos.ny >= 0 && newPos.ny < grid.Length)
+                    {
+                        minHeap.Insert((item.HL + grid[newPos.ny][newPos.nx], newPos.nx, newPos.ny, item.DirX, item.DirY, item.Steps + 1));
+                    }
+                }
+
+                if (item.Steps >= 4 || (item.DirX, item.DirY) == (0, 0))
+                {
+                    foreach (var dir in dirs)
+                    {
+                        if (dir != (item.DirX, item.DirY) && dir != (-item.DirX, -item.DirY))
+                        {
+                            (int nx, int ny) newPos = (item.X + dir.Item1, item.Y + dir.Item2);
+
+                            if (newPos.nx >= 0 && newPos.nx < grid[0].Length && newPos.ny >= 0 && newPos.ny < grid.Length)
+                            {
+                                minHeap.Insert((item.HL + grid[newPos.ny][newPos.nx], newPos.nx, newPos.ny, dir.Item1, dir.Item2, 1));
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return 0;
+        }
+
+    }
+}
