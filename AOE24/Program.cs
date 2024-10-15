f7345d3db13e0c3a3ddf2b0e40a0ccad9c3be154
Izabela Wiatrowska <33397049+3elk4@users.noreply.github.com>
2024-10-15 17:59:23 +0200

    add day 24 and 25 of aoe

---
+++ AOE24/Program.cs
@@ -0,0 +1,145 @@
+﻿namespace AOE24
+{
+    public static class DecimalExtention
+    {
+        public static bool InRange(this decimal value, long min, long max) => value >= min && value <= max;
+    }
+
+    public static class IntersectionExtention
+    {
+        private static long MinRange = 200000000000000;
+        private static long MaxRange = 400000000000000;
+
+        public static bool RangeIntersection(this Hailstone h1, Hailstone h2)
+        {
+            var result = h1.Intersection(h2);
+
+            if (!result.HasValue) return false;
+
+            return result.Value.X.InRange(MinRange, MaxRange) && result.Value.Y.InRange(MinRange, MaxRange) &&
+                    h1.FuturePoint(result.Value.X, result.Value.Y) && h2.FuturePoint(result.Value.X, result.Value.Y);
+        }
+
+        private static bool FuturePoint(this Hailstone h, decimal x, decimal y)
+        {
+            return (x - h.Position.X) * h.Velocity.X >= 0 && (y - h.Position.Y) * h.Velocity.Y >= 0;
+        }
+    }
+
+    public record Vec3(decimal X, decimal Y, decimal Z);
+
+    public class Hailstone
+    {
+        public Vec3 Position;
+        public Vec3 Velocity;
+
+        public decimal A => Velocity.Y;
+        public decimal B => -Velocity.X;
+        public decimal C => Velocity.Y * Position.X - Velocity.X * Position.Y;
+
+        public Hailstone(Vec3 position, Vec3 velocity)
+        {
+            this.Position = position;
+            this.Velocity = velocity;
+        }
+
+        public override string ToString()
+        {
+            return $"Position: ({Position.X},{Position.Y},{Position.Z}), Velocity: ({Velocity.X},{Velocity.Y},{Velocity.Z})";
+        }
+
+        public (decimal X, decimal Y)? Intersection(Hailstone other)
+        {
+            if (this.A * other.B == this.B * other.A)
+                return null;
+
+            decimal x = (this.C * other.B - other.C * this.B) / (this.A * other.B - other.A * this.B);
+            decimal y = (other.C * this.A - this.C * other.A) / (this.A * other.B - other.A * this.B);
+
+            return (x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result1 = 0;
+
+            string fileloc = @"data\data.txt";
+
+            var hailstones = PreprocessInput(fileloc);
+
+            //part1
+            result1 = IntersectionXYCount(hailstones);
+            Console.WriteLine(result1);
+
+            //part2
+            Console.WriteLine(StoneResultSearch(hailstones.ToList()));
+        }
+
+        static IEnumerable<Hailstone> PreprocessInput(string fileloc)
+        {
+            return File.ReadAllLines(fileloc)
+                .Select(line =>
+                {
+                    decimal[] positions = line
+                                     .Replace("@", ",")
+                                     .Split(",")
+                                     .Select(x => decimal.Parse(x))
+                                     .ToArray();
+
+                    return new Hailstone(new Vec3(positions[0], positions[1], positions[2]),
+                                    new Vec3(positions[3], positions[4], positions[5]));
+                });
+        }
+
+        static int IntersectionXYCount(IEnumerable<Hailstone> hailstones) =>
+            Enumerable.Range(0, hailstones.Count())
+                .Select(h1Idx =>
+                    Enumerable.Range(h1Idx + 1, hailstones.Count() - h1Idx - 1)
+                    .Count(h2Idx => h1Idx != h2Idx && hailstones.ElementAt(h1Idx).RangeIntersection(hailstones.ElementAt(h2Idx)))
+                )
+                .Sum();
+
+        static decimal StoneResultSearch(List<Hailstone> hailstones)
+        {
+            var translateXY = (Hailstone h, (decimal, decimal) vel) => new Hailstone(h.Position, new Vec3(h.Velocity.X - vel.Item1, h.Velocity.Y - vel.Item2, h.Velocity.Z));
+            var translateXZ = (Hailstone h, (decimal, decimal) vel) => new Hailstone(new Vec3(h.Position.X, h.Position.Z, h.Position.Y), new Vec3(h.Velocity.X - vel.Item1, h.Velocity.Z - vel.Item2, h.Velocity.Y));
+
+            var resultXY = Solve2D(hailstones, translateXY);
+            var resultXZ = Solve2D(hailstones, translateXZ);
+
+            return resultXY.p1 + resultXY.p2 + resultXZ.p2;
+        }
+
+        static bool Hits(Hailstone h, (decimal X, decimal Y)? intRes)
+        {
+            var d = (intRes.Value.X - h.Position.X) * h.Velocity.Y - (intRes.Value.Y - h.Position.Y) * h.Velocity.X;
+            return Math.Abs(d) < (decimal)0.0001;
+        }
+
+        private static (decimal p1, decimal p2) Solve2D(List<Hailstone> hailstones, Func<Hailstone, (decimal, decimal), Hailstone> translate)
+        {
+            int s = 300;
+
+            for (var v1 = -s; v1 < s; v1++)
+            {
+                for (var v2 = -s; v2 < s; v2++)
+                {
+                    (decimal, decimal) vel = (v1, v2);
+
+                    var result = translate(hailstones[0], vel).Intersection(translate(hailstones[1], vel));
+
+                    if (!result.HasValue) continue;
+
+                    if (hailstones.All(h => Hits(translate(h, vel), result)))
+                    {
+                        return (result.Value.X, result.Value.Y);
+                    }
+                }
+            }
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ AOE25/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace AOE25
+{
+    public static class DictionaryExtention
+    {
+        public static void Add<T, U>(this Dictionary<T, List<U>> dict, T key, U value)
+        {
+            if (!dict.ContainsKey(key))
+                dict[key] = new();
+
+            dict[key].Add(value);
+        }
+
+        public static void Rebind(this Dictionary<string, List<string>> graph, string oldNode, string newNode)
+        {
+            foreach (var n in graph[oldNode])
+            {
+                while (graph[n].Remove(oldNode))
+                {
+                    graph[n].Add(newNode);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        private static Random r = new Random(127);
+
+        static void Main(string[] args)
+        {
+            string fileloc = @"data\data.txt";
+
+            var (cutSize, c1, c2) = FindCut(fileloc);
+
+            while (cutSize != 3)
+            {
+                (cutSize, c1, c2) = FindCut(fileloc);
+            }
+
+            Console.WriteLine(c1 * c2);
+        }
+
+        private static Dictionary<string, List<string>> PreprocessInput(string fileloc)
+        {
+            var graph = new Dictionary<string, List<string>>();
+
+            File.ReadAllLines(fileloc).ToList()
+                .ForEach(line =>
+                {
+                    var elements = line.Replace(":", "").Split(" ");
+
+                    for (int i = 1; i < elements.Length; ++i)
+                    {
+                        graph.Add(elements[0], elements[i]);
+                        graph.Add(elements[i], elements[0]);
+                    }
+                });
+
+            return graph;
+        }
+
+        private static (int size, int c1, int c2) FindCut(string fileloc)
+        {
+            var graph = PreprocessInput(fileloc);
+            var nodeSize = graph.Keys.ToDictionary(k => k, _ => 1);
+
+            while (graph.Count > 2)
+            {
+                var u = graph.Keys.ElementAt(r.Next(graph.Count));
+                var v = graph[u][r.Next(graph[u].Count)];
+
+                var merged = $"merge-{u}-{v}";
+
+                graph[merged] = graph[u].Where(x => x != v).Concat(graph[v].Where(x => x != u)).ToList();
+
+                graph.Rebind(u, merged);
+                graph.Rebind(v, merged);
+
+                nodeSize[merged] = nodeSize[u] + nodeSize[v];
+
+                graph.Remove(u);
+                graph.Remove(v);
+            }
+
+            var nodeA = graph.Keys.First();
+            var nodeB = graph.Keys.Last();
+            return (graph[nodeA].Count(), nodeSize[nodeA], nodeSize[nodeB]);
+        }
+    }
+}
